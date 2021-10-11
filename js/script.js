b321c90872b3eb3e50e22a706f55bec6168f31c9
Aituar Adalbayev <aituar.adalbayev@gmail.com>
2021-10-11 23:13:57 +0600

    add mobile menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const schemeSvg = document.querySelector(".scheme-svg");
 const totalPriceTag = document.querySelector(".price-total");
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
 let cost = 800; //цена билета 800 тенге
 let totalPrice = 0; //по умолчанию 0, а потом будет увеличиваться
 //Клик от пользователя Add event listener
@@ -12,4 +14,9 @@ schemeSvg.addEventListener('click', (event)=> {
     totalPrice = totalSeats * cost; 
     totalPriceTag.textContent = totalPrice;
 }
+});
+menuButton.addEventListener('click', () => {
+    console.log("Клик по меню");
+    menu.classList.toggle('is-open'); /*когда меню надо показывать */
+  
 });
